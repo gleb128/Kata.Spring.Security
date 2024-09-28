af3bbd52888fc1a7592caf6deb7170e86f48ce34
gleb128 <glebtide@gmail.com>
2024-09-29 00:51:50 +0300

    перед изменением webSecutiryConfig

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -116,12 +116,12 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
