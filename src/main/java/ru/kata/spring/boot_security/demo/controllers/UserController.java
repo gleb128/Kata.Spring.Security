5c41978b11d12a68b8cda5ce4f03247b7763f342
gleb128 <glebtide@gmail.com>
2024-10-02 03:16:12 +0300

    почти закончил

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/user")
+    public String userInfo (Principal principal,Model model) {
+        User user = (User) userService.loadUserByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "User_Info";
+    }
+}
